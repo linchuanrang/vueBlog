05f41c33242d9f6d3cc2a768679a621582bca9df
unknown <1808602051@qq.com>
2020-02-23 17:55:09 +0800

    rang

---
+++ src/config/data/one.js
@@ -0,0 +1,114 @@
+export const Article = require("./Article.json")
+export const ArticleList = require("./ArticleList.json")
+export const Logolink = require("./Header.json")
+
+
+
+
+
+
+
+
+/* 
+// import data from ""
+
+// index.config
+// import ArticleListFile from './ArticleList.json';
+// cdn基路径
+const WWW = 'https://www.fudaqiang.com';
+const CDN = 'https://cdn.jsdelivr.net/gh/qiang520184/cdn@1.7.1';
+const Logo = '/images/rooon.gif';
+const ImgAlt = '付大强';
+//
+export const Logolink = [
+    {
+        title: 'さくら荘その'
+    },
+    {
+        title: '让い'
+    }
+];
+// 首页轮播图
+export const Banner = [
+    {
+        img: `${CDN}/images/cover/1.jpg`
+    },
+    {
+        img: `${CDN}/images/cover/2.jpg`
+    },
+    {
+        img: `${CDN}/images/cover/3.jpg`
+    },
+    {
+        img: `${CDN}/images/cover/4.jpg`
+    },
+    {
+        img: `${CDN}/images/cover/5.jpg`
+    },
+    {
+        img: `${CDN}/images/cover/6.jpg`
+    }
+];
+// 首页列表
+// export const ArticleList = ArticleListFile;
+export const FocusInfo = [
+    {
+        title: `If you haven't found it yet, keep looking and don't settle${''}`,
+        txt: '如果你还没找到，那继续找，别安逸下来。'
+    },
+    {
+        title: `Live your life with passion! With some drive!${''}`,
+        txt: '充满激情地生活！开着车！'
+    }
+];
+export const FocusInfoIcon = [
+    {
+        img: `${CDN}/img/other/next-b.svg`,
+        text: 'left',
+        type: 'left'
+    },
+    {
+        img: `${CDN}/img/social/github.png`,
+        path: 'https://github.com/qiang520184',
+        text: 'github',
+        type: '_blank'
+    },
+    {
+        img: `${CDN}/img/social/email.svg`,
+        path: 'mailto:199513521@qq.com',
+        text: 'email',
+        type: '_blank'
+    },
+    {
+        img: `${CDN}/img/social/sina.png`,
+        path: 'https://weibo.com/xq520184',
+        text: 'sina',
+        type: '_blank'
+    },
+    {
+        img: `${CDN}/img/social/wangyiyun.png`,
+        path: 'https://music.163.com/#/playlist?id=2523258784',
+        type: 'hover',
+        hoverImg: `${CDN}/img/custom/donate/wxgzh.jpg`
+    },
+    {
+        img: `${CDN}/img/social/wechat.png`,
+        text: 'weixin',
+        type: 'hover',
+        hoverImg: `${CDN}/img/custom/donate/wxgzh.jpg`
+    },
+    {
+        img: `${CDN}/img/other/next-b.svg`,
+        text: 'right',
+        type: 'right'
+    }
+];
+// 基本信息
+export const defaultInfo = {
+    cdn: CDN,
+    logo: Logo,
+    www: WWW,
+    imgAlt: ImgAlt
+};
+
+*/
